6a01077971ff30682aed21dd005694f92ed91599
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-12-04 12:34:38 +0300

    Update settings.py

--- SMS/settings.py
+++ SMS/settings.py
@@ -175,7 +175,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['staticfiles']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
